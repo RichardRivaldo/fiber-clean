b5f72bef37351263db444e14c615e1019996f390
RichardRivaldo <richardrivaldo84@gmail.com>
2023-05-10 20:48:49 +0700

    feat: updated model for user and course

--- src/models/course_model.go
+++ src/models/course_model.go
@@ -1,13 +1,17 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"mime/multipart"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Course struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Name      string             `bson:"name,omitempty" validate:"email,required"`
-	Category  string             `bson:"category,omitempty" validate:"required"`
-	Price     bool               `bson:"price,omitempty" validate:"required"`
-	Details   string             `bson:"details,omitempty"`
-	ImageLink string             `bson:"image_link,omitempty"`
-	IsDeleted bool               `bson:"is_deleted,omitempty" validate:"required"`
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	Name     string             `bson:"name,omitempty" validate:"required"`
+	Category string             `bson:"category,omitempty" validate:"required"`
+	Price    int                `bson:"price,omitempty" validate:"required"`
+	Details  string             `bson:"details,omitempty"`
+	Image    multipart.File     `bson:"image,omitempty"`
+	Exists   bool               `bson:"exists,omitempty"`
 }
--- src/models/user_model.go
+++ src/models/user_model.go
@@ -6,5 +6,5 @@ type User struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty"`
 	Email    string             `bson:"email,omitempty" validate:"email,required"`
 	Password string             `bson:"password,omitempty" validate:"required"`
-	Exists   bool               `bson:"exists,omitempty" validate:"required"`
+	Exists   bool               `bson:"exists,omitempty"`
 }
